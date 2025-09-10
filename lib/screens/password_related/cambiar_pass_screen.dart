7bf4a894b4fb20f993e3c72bc369020e8fd48a68
sistemasunicab <sistemasunicab@gmail.com>
2025-09-10 18:31:53 -0500

    ajustes pass y cumpleaños

--- lib/screens/password_related/cambiar_pass_screen.dart
+++ lib/screens/password_related/cambiar_pass_screen.dart
@@ -17,23 +17,18 @@ class CambiarPassScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(11, 119, 179, 1),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: constraints.maxHeight,
-              ),
-
-              child: IntrinsicHeight(
-                child: SafeArea(
-                  child: _MainBody()
-                ),
-              ),
+      body: LayoutBuilder(builder: (context, constraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight,
             ),
-          );
-        }
-      ),
+            child: IntrinsicHeight(
+              child: SafeArea(child: _MainBody()),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
@@ -45,47 +40,37 @@ class _MainBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
-
       children: [
         MainNavegationBar(navType: NavBarTypes.noBody),
-
         SizedBox(height: 10),
-        
         Image.asset(
           'assets/img/LOGO UNICONECTA.png',
           height: 54,
           width: 154,
         ),
-
         Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: [
-              Text(
-                "Vamos a cambiar tu clave secreta.",
-                style: TextStyle(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Vamos a cambiar tu clave secreta.",
+              style: TextStyle(
                   color: Colors.white,
                   fontWeight: FontWeight.w700,
                   fontFamily: "Roboto",
-                  fontSize: 17.sp
-                ),
-              ),
-
-              Text(
-                "Shhh, ¡que nadie se entere cual es!",
-                style: TextStyle(
+                  fontSize: 17.sp),
+            ),
+            Text(
+              "Shhh, ¡que nadie se entere cual es!",
+              style: TextStyle(
                   color: const Color.fromARGB(255, 221, 221, 221),
                   fontWeight: FontWeight.w400,
                   fontFamily: "Roboto",
-                  fontSize: 16.sp
-                ),
-              ),
-
-              _MainForm()
-            ],
-          )
-        )
+                  fontSize: 16.sp),
+            ),
+            _MainForm()
+          ],
+        ))
       ],
     );
   }
@@ -99,7 +84,6 @@ class _MainForm extends StatefulWidget {
 }
 
 class _MainFormState extends State<_MainForm> {
-
   String? errorMsg;
   bool _loadingRequest = false;
   final _formKey = GlobalKey<FormState>();
@@ -128,64 +112,58 @@ class _MainFormState extends State<_MainForm> {
     UserProvider provider = Provider.of<UserProvider>(context, listen: false);
 
     try {
-      CambiarPassResponse response = await CambiarPasswordRepository.sendRequest(
+      CambiarPassResponse response =
+          await CambiarPasswordRepository.sendRequest(
         email: provider.user!.email,
         role: provider.user!.userRole,
-
         newPassword: mainController.text.trim(),
       );
 
       if (!context.mounted) return;
 
       if (response.status) {
-        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (_) => GeneralScreen()), (Route<dynamic> route) => false);
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (_) => GeneralScreen()),
+            (Route<dynamic> route) => false);
 
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            backgroundColor: const Color.fromRGBO(11, 119, 179, 1), 
-            content: Row(
-              mainAxisAlignment: MainAxisAlignment.center, 
-              children: [
+              backgroundColor: const Color.fromRGBO(11, 119, 179, 1),
+              content:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                 Icon(Icons.check_circle_rounded, color: Colors.white),
                 SizedBox(width: 4),
-
-                Text(response.mensaje, style: TextStyle(fontFamily: "Roboto", fontSize: 15.sp, color: Colors.white))
-              ]
-            )
-          ),
+                Text(response.mensaje,
+                    style: TextStyle(
+                        fontFamily: "Roboto",
+                        fontSize: 15.sp,
+                        color: Colors.white))
+              ])),
         );
-
       } else {
-
         setState(() {
           errorMsg = response.mensaje;
         });
-
       }
-
     } catch (e) {
-
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          backgroundColor: const Color.fromRGBO(11, 119, 179, 1), 
-          content: Row(
-            mainAxisAlignment: MainAxisAlignment.center, 
-            children: [
+            backgroundColor: const Color.fromRGBO(11, 119, 179, 1),
+            content:
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               Icon(Icons.error_rounded, color: Colors.white),
               SizedBox(width: 4),
-
-              Text("Ha Ocurrido un error, Revisa tu conexion a internet.", style: TextStyle(fontFamily: "Roboto", fontSize: 15.sp, color: Colors.white))
-            ]
-          )
-        ),
+              Text("Ha Ocurrido un error, Revisa tu conexion a internet.",
+                  style: TextStyle(
+                      fontFamily: "Roboto",
+                      fontSize: 15.sp,
+                      color: Colors.white))
+            ])),
       );
-
     } finally {
-      
       setState(() {
         _loadingRequest = false;
       });
-    
     }
   }
 
@@ -196,65 +174,65 @@ class _MainFormState extends State<_MainForm> {
       child: Column(
         children: [
           if (errorMsg != null) ...[
-              SizedBox(height: 10),
-
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 22.sp, horizontal: 33.sp),
-                padding: EdgeInsets.all(18),
-                decoration: BoxDecoration(
+            SizedBox(height: 10),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 22.sp, horizontal: 33.sp),
+              padding: EdgeInsets.all(18),
+              decoration: BoxDecoration(
                   color: Color.fromRGBO(246, 119, 144, 1),
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30), bottomRight: Radius.circular(30)),
-                  border: Border.all(color: Color.fromRGBO(255, 23, 68, 1), width: 2)
-                ),
-
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      errorMsg!,
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 15.sp,
-                        color: Colors.white,
-                        fontFamily: 'Roboto',
-                      ),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                      bottomRight: Radius.circular(30)),
+                  border: Border.all(
+                      color: Color.fromRGBO(255, 23, 68, 1), width: 2)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    errorMsg!,
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15.sp,
+                      color: Colors.white,
+                      fontFamily: 'Roboto',
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ] else SizedBox(height: 75),
-
+            ),
+          ] else
+            SizedBox(height: 75),
           CampoSesion(
-            fieldLabel: "Nueva Contraseña", 
+            fieldLabel: "Nueva Contraseña",
             fieldPlaceholder: "¿Cual sera tu nueva clave secreta?",
-          
+            pass: "SI",
             controller: mainController,
             fieldChecking: CustomFormFieldValidator.password,
             validator: (String? value) {
               if (value != null && value.isEmpty) {
                 return "Ingresa un valor.";
-              } else { 
-                return CustomFormFieldValidator.correo(value, esRequerido: true);
+              } else {
+                return CustomFormFieldValidator.correo(value,
+                    esRequerido: true);
               }
-            }, 
+            },
           ),
-
           SizedBox(height: 50),
-
-          (!_loadingRequest) 
+          (!_loadingRequest)
               ? OrangeButton(
                   onPressed: () => _sendRequest(context),
-
                   buttonText: "Actualizar",
                   textWeight: FontWeight.w600,
-                  padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 2.h), 
+                  padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 2.h),
                 )
-
-              : SizedBox(height: 26, width: 26, child: CircularProgressIndicator(color: Colors.white)),
+              : SizedBox(
+                  height: 26,
+                  width: 26,
+                  child: CircularProgressIndicator(color: Colors.white)),
         ],
       ),
     );
   }
-
 }
